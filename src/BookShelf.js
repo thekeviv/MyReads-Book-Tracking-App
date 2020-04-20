79dcfc554f63f649635248545e2a44cca35e4e33
Vivek Sharma <thekeviv.vivek@gmail.com>
2020-04-19 22:19:14 -0700

    Implemented BookShelf

---
+++ src/BookShelf.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Book from "./Book";
+class BookShelf extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired
+  };
+
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.book.map(book => (
+              <li key={book.name}>
+                <Book
+                  name={book.name}
+                  author={book.author}
+                  coverUrl={book.coverUrl}
+                />
+              </li>
+            ))}
+            <li />
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookShelf;
